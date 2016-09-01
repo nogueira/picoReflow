2b4cea52827b2cdf3aaee6e3e1a88444fbf238e8
Octavio Nogueira <nogueira@tato.ind.br>
2016-09-01 10:46:57 -0300

    teste 6675

--- lib/max6675.py
+++ lib/max6675.py
@@ -23,9 +23,9 @@ class MAX6675(object):
         - board:     (optional) pin numbering method as per RPi.GPIO library (GPIO.BCM (default) | GPIO.BOARD)
 
         '''
-        self.cs_pin = port.PA9 #cs_pin
-        self.clock_pin = port.PA8 #clock_pin
-        self.data_pin = port.PA21 #data_pin
+        self.cs_pin = cs_pin #port.PA9 
+        self.clock_pin = clock_pin #port.PA8 
+        self.data_pin = data_pin #port.PA21 #
         self.units = units
         self.data = None
         #self.board = board
